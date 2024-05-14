d4b1f54abcfe85ec8aaf5664441b4e43b3f04315
StanlyDev <stanvsdev@gmail.com>
2024-05-14 02:59:46 -0600

    update

--- theme-page.js
+++ theme-page.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const phrase = document.querySelector('.phrase');
     const myinfo = document.querySelector('.myinfo');
     const modoOscuroCheckbox = document.getElementById('modoOscuro');
+    const listaLi = document.querySelectorAll('.op-page li'); // selecciona todos los elementos li dentro de .op-page
 
     modoOscuroCheckbox.addEventListener('change', function () {
         if (modoOscuroCheckbox.checked) {
@@ -11,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
             myname.style.color = 'black';
             phrase.style.color = 'black';
             myinfo.style.color = '#495057';
+            listaLi.forEach(li => {
+                li.style.color = '#495057';
+            });
             const icons = myinfo.querySelectorAll('lord-icon');
             icons.forEach(icon => {
                 icon.setAttribute('colors', 'primary:#495057');
@@ -20,6 +24,9 @@ document.addEventListener('DOMContentLoaded', function () {
             myname.style.color = '#fff';
             phrase.style.color = '#fff';
             myinfo.style.color = '#b4b4b4';
+            listaLi.forEach(li => {
+                li.style.color = '#b4b4b4';
+            });
             const icons = myinfo.querySelectorAll('lord-icon');
             icons.forEach(icon => {
                 icon.setAttribute('colors', 'primary:#fff');
