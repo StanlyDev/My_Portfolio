0f32cb05b286e24e918d8dd7b07962afd06278f9
StanlyDev <stanvsdev@gmail.com>
2024-05-14 00:35:44 -0600

    update

--- theme-page.js
+++ theme-page.js
@@ -1,30 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const sunButton = document.querySelector('.light-theme');
-    const moonButton = document.querySelector('.dark-theme');
     const body = document.body;
     const myname = document.querySelector('.myname');
     const phrase = document.querySelector('.phrase');
     const myinfo = document.querySelector('.myinfo');
+    const modoOscuroCheckbox = document.getElementById('modoOscuro');
 
-    sunButton.addEventListener('click', function () {
-        body.style.backgroundColor = '#fff';
-        myname.style.color = 'black';
-        phrase.style.color = 'black';
-        myinfo.style.color = '#495057';
-        const icons = myinfo.querySelectorAll('lord-icon');
-        icons.forEach(icon => {
-            icon.setAttribute('colors', 'primary:#495057');
-        });
-    });
-
-    moonButton.addEventListener('click', function () {
-        body.style.backgroundColor = '#212121';
-        myname.style.color = '#fff';
-        phrase.style.color = '#fff';
-        myinfo.style.color = '#b4b4b4';
-        const icons = myinfo.querySelectorAll('lord-icon');
-        icons.forEach(icon => {
-            icon.setAttribute('colors', 'primary:#fff');
-        });
+    modoOscuroCheckbox.addEventListener('change', function () {
+        if (modoOscuroCheckbox.checked) {
+            body.style.backgroundColor = '#fff';
+            myname.style.color = 'black';
+            phrase.style.color = 'black';
+            myinfo.style.color = '#495057';
+            const icons = myinfo.querySelectorAll('lord-icon');
+            icons.forEach(icon => {
+                icon.setAttribute('colors', 'primary:#495057');
+            });
+        } else {
+            body.style.backgroundColor = '#212121';
+            myname.style.color = '#fff';
+            phrase.style.color = '#fff';
+            myinfo.style.color = '#b4b4b4';
+            const icons = myinfo.querySelectorAll('lord-icon');
+            icons.forEach(icon => {
+                icon.setAttribute('colors', 'primary:#fff');
+            });
+        }
     });
 });
